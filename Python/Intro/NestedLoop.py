6cb38e3aea97cd09f52bebc6703f86b2096ac903
M <honeybhogadi@gmail.com>
2023-07-26 17:06:57 -0700

    nested loops

--- Python/Intro/NestedLoop.py
+++ Python/Intro/NestedLoop.py
@@ -16,11 +16,11 @@
 # 1 2 3 4 5
 
 
-# n = int(input())
-# for i in range(1, n+1):
-#     for j in range(1, i+1):
-#         print(j, end=" ")
-#     print(" ")
+n = int(input())
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print(" ")
 
 
 # 1 2 3 4 5
